343f78db8975cbd76a56a3a27dcb13fd39fd3a0e
yangzhichina <yangzhichina@gmail.com>
2013-08-03 15:10:40 +0800

    Logs added.

--- main.cpp
+++ main.cpp
@@ -12,12 +12,12 @@ extern "C" FILE *yyin;
 
 int main(int argc, char* argv[])
 {
-    //yydebug = 1;
-
     FILE* f = fopen("input.txt", "r");
     if (!f)
     {
         std::cout << "No file found." << std::endl;
+        // Don't delete programBlock here, it's not created.
+        // By the way, it is created after yyparse().
         return -1;
     }
 
@@ -29,7 +29,7 @@ int main(int argc, char* argv[])
 
     fclose(f);
 
-    delete programBlock;
+    delete programBlock; // It's right position for deletion.
 
     return 0;
 }
